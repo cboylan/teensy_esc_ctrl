e20ac5f56c314c5f105f3404567c42ed562be159
Clark Boylan <clark.boylan@gmail.com>
2010-11-18 20:38:39 -0800

    Rip out usb serial code and put esc control into an infinite loop that increases then decreases throttle.

--- esc_control.c
+++ esc_control.c
@@ -23,13 +23,8 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 #include <util/delay.h>
-#include "usb_serial.h"
 
 #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
-
-void send_str(const char *s);
-uint8_t recv_str(char *buf, uint8_t size);
-void parse_and_execute_command(const char *buf, uint8_t num);
 
 static const uint16_t icr_val = 40000;
 static const uint16_t min_ocr = 2000;
@@ -93,11 +88,6 @@ void setup_pwms(void)
     DDRC |= (1 << DDC6);
 }
 
-void handle_query_pwm_command(uint8_t port)
-{
-	// TODO
-}
-
 /**
  * @breif Handle a set pwm command
  */
@@ -105,12 +95,8 @@ void handle_set_pwm_command(uint8_t port, uint8_t vall, uint8_t valh)
 {
     /* TODO: verify new duty cycle is within valid range. */
     if( (valh < min_ocrh && vall < min_ocrl) || (valh > max_ocrh && vall > max_ocrl) ){
-	    send_str(PSTR("Invalid PWM value. Must be between 2000 and 4000 (inclusive).\n"));
         return;
     }
-
-	usb_serial_putchar('\x04');
-	usb_serial_putchar(port);
 
 	switch(port)
 	{
@@ -131,38 +117,6 @@ void handle_set_pwm_command(uint8_t port, uint8_t vall, uint8_t valh)
 			OCR3AL = vall;
 			break;
 	}
-
-	usb_serial_putchar('\x00');
-	usb_serial_putchar('\n');
-}
-
-void handle_version_command(void)
-{
-	usb_serial_putchar('\x00');
-	send_str(PSTR("ESC Controller 1.0\n"));
-}
-
-void handle_ping_command(const char *str, uint8_t len)
-{
-	const char *data_itr;
-	uint8_t ndx = 1;
-
-	data_itr = str;
-
-	usb_serial_putchar('\x02');
-	while(ndx < len)
-	{
-		usb_serial_putchar(data_itr[ndx]);
-		++ndx;
-	}
-	usb_serial_putchar('\n');
-}
-
-void handle_pwm_ports_command(void)
-{
-	usb_serial_putchar('\x03');
-	usb_serial_putchar('\x00');
-	send_str(PSTR("\x01\x02\x03\x04\x05\x06\x07\x08\n"));
 }
 
 void handle_arm_esc_command(uint8_t port)
@@ -195,170 +149,25 @@ void handle_arm_esc_command(uint8_t port)
     }
 }
 
-void handle_command(const char *str, uint8_t len)
-{
-	if(len == 0)
-		return;
-
-	switch(str[0])
-	{
-		case 0:
-			handle_version_command();
-			break;
-		case 1:
-			handle_ping_command(str, len);
-			break;
-		case 3:
-			handle_pwm_ports_command();
-			break;
-		case 4:
-			handle_set_pwm_command(str[1], str[2], str[3]);
-			break;
-		case 5:
-			handle_query_pwm_command(str[1]);
-			break;
-		case 6:
-			handle_arm_esc_command(str[1]);
-			break;
-		default:
-			send_str(PSTR("INVALID_COMMAND_CODE"));
-	}
-}
-
 int main(void)
 {
 	char buf[32];
 	uint8_t n;
+    uint16_t on_time;
 
 	CPU_PRESCALE(0);
 	setup_pwms();
 
-	usb_init();
-	while (!usb_configured()) /* wait */ ;
-	_delay_ms(1000);
+    handle_arm_esc_command(0);
 
 	while (1){
-		while (!(usb_serial_get_control() & USB_SERIAL_DTR)) /* wait */ ;
-		usb_serial_flush_input();
-
-		while (1) {
-			n = recv_str(buf, sizeof(buf));
-			if (n == 255) break;
-			handle_command(buf, n);
-		}
-	}
-
-}
-
-// Send a string to the USB serial port.  The string must be in
-// flash memory, using PSTR
-//
-void send_str(const char *s)
-{
-	char c;
-	while (1) {
-		c = pgm_read_byte(s++);
-		if (!c) break;
-		usb_serial_putchar(c);
+        for(on_time = min_ocr; on_time <= max_ocr; on_time += 10 ){
+            handle_set_pwm_command(0, on_time, on_time >> 8);
+            _delay_ms(100);
+        }
+        for(on_time = max_ocr; on_time >= min_ocr; on_time -= 10 ){
+            handle_set_pwm_command(0, on_time, on_time >> 8);
+            _delay_ms(100);
+        }
 	}
 }
-
-// Receive a string from the USB serial port.  The string is stored
-// in the buffer and this function will not exceed the buffer size.
-// A carriage return or newline completes the string, and is not
-// stored into the buffer.
-// The return value is the number of characters received, or 255 if
-// the virtual serial connection was closed while waiting.
-//
-uint8_t recv_str(char *buf, uint8_t size)
-{
-	int16_t r;
-	uint8_t count=0;
-
-	while (count < size) {
-		r = usb_serial_getchar();
-		if (r != -1) {
-			if (r == '\r' || r == '\n') return count;
-			*buf++ = r;
-			//usb_serial_putchar(r);
-			count++;
-		} else {
-			if (!usb_configured() ||
-			  !(usb_serial_get_control() & USB_SERIAL_DTR)) {
-				// user no longer connected
-				return 255;
-			}
-			// just a normal timeout, keep waiting
-		}
-	}
-	return count;
-}
-
-// parse a user command and execute it, or print an error message
-//
-void parse_and_execute_command(const char *buf, uint8_t num)
-{
-	uint8_t port, pin, val;
-
-	if (num < 3) {
-		send_str(PSTR("unrecognized format, 3 chars min req'd\r\n"));
-		return;
-	}
-	// first character is the port letter
-	if (buf[0] >= 'A' && buf[0] <= 'F') {
-		port = buf[0] - 'A';
-	} else if (buf[0] >= 'a' && buf[0] <= 'f') {
-		port = buf[0] - 'a';
-	} else {
-		send_str(PSTR("Unknown port \""));
-		usb_serial_putchar(buf[0]);
-		send_str(PSTR("\", must be A - F\r\n"));
-		return;
-	}
-	// second character is the pin number
-	if (buf[1] >= '0' && buf[1] <= '7') {
-		pin = buf[1] - '0';
-	} else {
-		send_str(PSTR("Unknown pin \""));
-		usb_serial_putchar(buf[0]);
-		send_str(PSTR("\", must be 0 to 7\r\n"));
-		return;
-	}
-	// if the third character is a question mark, read the pin
-	if (buf[2] == '?') {
-		// make the pin an input
-		*(uint8_t *)(0x21 + port * 3) &= ~(1 << pin);
-		// read the pin
-		val = *(uint8_t *)(0x20 + port * 3) & (1 << pin);
-		usb_serial_putchar(val ? '1' : '0');
-		send_str(PSTR("\r\n"));
-		return;
-	}
-	// if the third character is an equals sign, write the pin
-	if (num >= 4 && buf[2] == '=') {
-		if (buf[3] == '0') {
-			// make the pin an output
-			*(uint8_t *)(0x21 + port * 3) |= (1 << pin);
-			// drive it low
-			*(uint8_t *)(0x22 + port * 3) &= ~(1 << pin);
-			return;
-		} else if (buf[3] == '1') {
-			// make the pin an output
-			*(uint8_t *)(0x21 + port * 3) |= (1 << pin);
-			// drive it high
-			*(uint8_t *)(0x22 + port * 3) |= (1 << pin);
-			return;
-		} else {
-			send_str(PSTR("Unknown value \""));
-			usb_serial_putchar(buf[3]);
-			send_str(PSTR("\", must be 0 or 1\r\n"));
-			return;
-		}
-	}
-	// otherwise, error message
-	send_str(PSTR("Unknown command \""));
-	usb_serial_putchar(buf[0]);
-	send_str(PSTR("\", must be ? or =\r\n"));
-}
-
-
